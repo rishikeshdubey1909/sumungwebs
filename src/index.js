9b32799fc07c8fdaaf29a996ea58c09f5acdece0
rishikeshdubey1909 <rishikeshdubey1909@gmail.com>
2026-03-01 23:43:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SumungWaitlist from './SumungWaitlist';
+import SumungWaitlist from './sumung-waitlist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
